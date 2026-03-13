f23f8550a4e574f58db0cd2b10ab702f741a4fde
Bohdan Mykyta <justkexitmail@gmail.com>
2026-03-13 14:53:43 +0200

    Fix test problems

--- car/serializers.py
+++ car/serializers.py
@@ -3,12 +3,12 @@ from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(required=False)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
     horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True)
+    problem_description = serializers.CharField(required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
